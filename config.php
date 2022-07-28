304d8977f14333841b8f45a1c8ebeaadecf0ecf8
Rajnish Kumar <107612148+rajnishaspireindia@users.noreply.github.com>
2022-07-28 12:32:37 +0530

    Update config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 $dbHost     = "localhost"; 
 $dbUsername = "root"; 
 $dbPassword = ""; 
-$dbName     = "bakersoven"; 
+$dbName     = "abc"; //database name
  
 // Create database connection 
 $db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName); 
